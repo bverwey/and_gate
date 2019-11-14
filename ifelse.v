cb9048cc28939fa81fd0de416356fbcde772b02f
bverwey <57469215+bverwey@users.noreply.github.com>
2019-11-13 18:34:19 -0800

    ifelse.v

---
+++ ifelse.v
@@ -0,0 +1,19 @@
+parameter off = 1'b0;
+parameter on  = 1'b1;
+
+module demo(
+  input control, i, output y
+);
+  
+  always@(control or i) begin
+    if (control == on)
+      y = i;
+      else y = 0;
+  end
+  endmodule
+    
+module ts_top(
+  output y
+);
+  demo uut(off, on, y);
+endmodule
